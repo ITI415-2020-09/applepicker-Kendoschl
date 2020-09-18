2178c1d77d272b5c8b185a11c6cc6b63c304cc00
Kendoschl <60159000+Kendoschl@users.noreply.github.com>
2020-09-18 17:54:16 -0400

    Reload Game when 0 Baskets

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ApplePicker : MonoBehaviour
 {
@@ -33,6 +34,11 @@ public class ApplePicker : MonoBehaviour
         GameObject tBasketGO = basketList[basketIndex];
         basketList.RemoveAt(basketIndex);
         Destroy(tBasketGO);
+
+        if(basketList.Count == 0)
+        {
+            SceneManager.LoadScene("ApplePicker");
+        }
     }
 
     // Update is called once per frame
